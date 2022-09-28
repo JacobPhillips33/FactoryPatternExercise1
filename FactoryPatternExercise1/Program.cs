47207a12f14aa11cbeece11b73c8884b0e1e064f
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-27 20:18:24 -0500

    initial commit

---
+++ FactoryPatternExercise1/Program.cs
@@ -0,0 +1,9 @@
+﻿using FactoryPatternExercise1;
+
+IVehicle vehicle1 = VehicleFactory.GetVehicle();
+vehicle1.Drive();
+
+Console.WriteLine();
+
+IVehicle vehicle2 = VehicleFactory.GetVehicle();
+vehicle2.Drive();
---
+++ FactoryPatternExercise1/VehicleFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise1
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle()
+        {           
+            
+            Console.Write("Will your vehicle have 2 or 4 tires? ");
+            
+            bool inputParseable = false;
+            int userTiresInput = 0;            
+            while (!inputParseable)
+            {
+                inputParseable = int.TryParse(Console.ReadLine(), out userTiresInput);
+
+                while (userTiresInput != 2 && userTiresInput != 4)
+                {                    
+                    Console.WriteLine("Invalid entry. Please try again.");
+                    Console.Write("How many tires will your vehicle have? ");
+                    inputParseable = int.TryParse(Console.ReadLine(), out userTiresInput);
+                }
+                
+            }
+
+            if (userTiresInput == 2)
+            {
+                return new Motorcycle();
+            }
+            return new Car();
+        }
+    }
+}
